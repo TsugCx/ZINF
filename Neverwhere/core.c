eb279a22e04f4e7fbc69063a49fe1472b4f67dbc
Guto <tsug.cx@gmail.com>
2025-05-22 16:22:53 -0300

    feat: Implemented function to display and navigate the main menu

---
+++ Neverwhere/core.c
@@ -0,0 +1,97 @@
+#include "core.h"
+#include "raylib.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stdint.h>
+#include <stdbool.h>
+
+void introduction_menu(){
+
+    int8_t select = 0;
+    bool continuos = true;
+
+    do{
+            if(IsKeyPressed(KEY_W)){
+                select--;
+                if(select < 0){
+                    select = 3;
+                }
+            }
+
+            if(IsKeyPressed(KEY_S)){
+                select++;
+                if(select > 4){
+                    select = 0;
+                }
+            }
+
+
+
+        switch(select) {
+
+            case 0:
+                draw_menu(select);
+
+                if(IsKeyPressed(KEY_ENTER))
+                    continuos = false;
+
+                break;
+
+            case 1:
+                draw_menu(select);
+                break;
+
+            case 2:
+                draw_menu(select);
+                break;
+
+            case 3:
+                draw_menu(select);
+
+                if(IsKeyPressed(KEY_ENTER))
+                   CloseWindow();
+                break;
+            }
+
+
+        } while(continuos);
+}
+
+void draw_menu(int select) {
+
+        BeginDrawing();
+
+        DrawText("ZINF",       400, 260, 180, BLUE);
+
+        DrawText("Iniciar",       500, 500, 25, WHITE);
+        DrawText("Placar",        500, 560, 25, WHITE);
+        DrawText("Configurações", 500, 620, 25, WHITE);
+        DrawText("Sair",          500, 680, 25, WHITE);
+
+        switch(select) {
+
+            case 0:
+                DrawRectangle(500, 530, 76, 4, BLUE);
+                break;
+
+            case 1:
+                DrawRectangle(500, 590, 68, 4, BLUE);
+                break;
+
+            case 2:
+                DrawRectangle(500, 650, 180, 4, BLUE);
+                break;
+
+            case 3:
+                DrawRectangle(500, 710, 50, 4, BLUE);
+                break;
+        }
+
+        ClearBackground(BLACK);
+        EndDrawing();
+
+}
+
+
---
+++ Neverwhere/core.h
@@ -0,0 +1,16 @@
+#ifndef CORE_H_INCLUDED
+#define CORE_H_INCLUDED
+
+//Cena de abertura introduzindo a historia do jogo, antes do menu principal propriamente, tem opção de ser pulada
+void introduction_scene();
+
+//Menu principal com opções para selecionar
+void introduction_menu();
+
+void draw_menu(int select);
+
+void settings();
+
+
+
+#endif
